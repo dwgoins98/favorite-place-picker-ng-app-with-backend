37a2f24ab0ca1c573b43c9bbd1c5172439734902
Daniel Goins <dwgoins98@gmail.com>
2025-02-18 16:55:09 -0500

    initial commit
    from Udemy course

---
+++ src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    selector: 'app-available-places',
+    templateUrl: './available-places.component.html',
+    styleUrl: './available-places.component.css',
+    imports: [PlacesComponent, PlacesContainerComponent]
+})
+export class AvailablePlacesComponent {
+  places = signal<Place[] | undefined>(undefined);
+}
